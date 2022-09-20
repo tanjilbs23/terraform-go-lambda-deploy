891fa7baeca03f8cbfa75a9148c3930c72322d5e
tanjilbs23 <tanjil.bs23@gmail.com>
2022-09-20 16:50:27 +0600

    go lambda test 1

--- main.tf
+++ main.tf
@@ -10,102 +10,106 @@ terraform {
 
 provider "aws" {}
 
-data "aws_caller_identity" "current" {}
-
-locals {
-  account_id      = data.aws_caller_identity.current.account_id
-  environment     = "dev"
-  lambda_handler  = "hello"
-  name            = "go-lambda-terraform-setup"
-  random_name     = "Morty"
-  region          = "eu-west-1"
+module "hello" {
+  source = "./lambda/hello"
 }
 
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_file = "../bin/hello"
-  output_path = "bin/hello.zip"
-}
+# data "aws_caller_identity" "current" {}
+
+# locals {
+#   account_id      = data.aws_caller_identity.current.account_id
+#   environment     = "dev"
+#   lambda_handler  = "hello"
+#   name            = "go-lambda-terraform-setup"
+#   random_name     = "Morty"
+#   region          = "eu-west-1"
+# }
+
+# data "archive_file" "lambda_zip" {
+#   type        = "zip"
+#   source_file = "../bin/hello"
+#   output_path = "bin/hello.zip"
+# }
 
 
-/*
-* IAM
-*/
+# /*
+# * IAM
+# */
 
-// Role
-data "aws_iam_policy_document" "assume_role" {
-  policy_id = "${local.name}-lambda"
-  version   = "2012-10-17"
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
+# // Role
+# data "aws_iam_policy_document" "assume_role" {
+#   policy_id = "${local.name}-lambda"
+#   version   = "2012-10-17"
+#   statement {
+#     effect  = "Allow"
+#     actions = ["sts:AssumeRole"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
+#     principals {
+#       type        = "Service"
+#       identifiers = ["lambda.amazonaws.com"]
+#     }
+#   }
+# }
 
-resource "aws_iam_role" "lambda" {
-  name                = "${local.name}-lambda"
-  assume_role_policy  = data.aws_iam_policy_document.assume_role.json
-}
+# resource "aws_iam_role" "lambda" {
+#   name                = "${local.name}-lambda"
+#   assume_role_policy  = data.aws_iam_policy_document.assume_role.json
+# }
 
-// Logs Policy
-data "aws_iam_policy_document" "logs" {
-  policy_id = "${local.name}-lambda-logs"
-  version   = "2012-10-17"
-  statement {
-    effect  = "Allow"
-    actions = ["logs:CreateLogStream", "logs:PutLogEvents"]
+# // Logs Policy
+# data "aws_iam_policy_document" "logs" {
+#   policy_id = "${local.name}-lambda-logs"
+#   version   = "2012-10-17"
+#   statement {
+#     effect  = "Allow"
+#     actions = ["logs:CreateLogStream", "logs:PutLogEvents"]
 
-    resources = [
-      "arn:aws:logs:${local.region}:${local.account_id}:log-group:/aws/lambda/${local.name}*:*"
-    ]
-  }
-}
+#     resources = [
+#       "arn:aws:logs:${local.region}:${local.account_id}:log-group:/aws/lambda/${local.name}*:*"
+#     ]
+#   }
+# }
 
-resource "aws_iam_policy" "logs" {
-  name   = "${local.name}-lambda-logs"
-  policy = data.aws_iam_policy_document.logs.json
-}
+# resource "aws_iam_policy" "logs" {
+#   name   = "${local.name}-lambda-logs"
+#   policy = data.aws_iam_policy_document.logs.json
+# }
 
-resource "aws_iam_role_policy_attachment" "logs" {
-  depends_on  = [aws_iam_role.lambda, aws_iam_policy.logs]
-  role        = aws_iam_role.lambda.name
-  policy_arn  = aws_iam_policy.logs.arn
-}
+# resource "aws_iam_role_policy_attachment" "logs" {
+#   depends_on  = [aws_iam_role.lambda, aws_iam_policy.logs]
+#   role        = aws_iam_role.lambda.name
+#   policy_arn  = aws_iam_policy.logs.arn
+# }
 
 
-/*
-* Cloudwatch
-*/
+# /*
+# * Cloudwatch
+# */
 
-// Log group
-resource "aws_cloudwatch_log_group" "log" {
-  name              = "/aws/lambda/${local.name}"
-  retention_in_days = 7
-}
+# // Log group
+# resource "aws_cloudwatch_log_group" "log" {
+#   name              = "/aws/lambda/${local.name}"
+#   retention_in_days = 7
+# }
 
-/*
-* Lambda
-*/
+# /*
+# * Lambda
+# */
 
-// Function
-resource "aws_lambda_function" "func" {
-  filename          = data.archive_file.lambda_zip.output_path
-  function_name     = local.name
-  role              = aws_iam_role.lambda.arn
-  handler           = local.lambda_handler
-  source_code_hash  = filebase64sha256(data.archive_file.lambda_zip.output_path)
-  runtime           = "go1.x"
-  memory_size       = 1024
-  timeout           = 30
+# // Function
+# resource "aws_lambda_function" "func" {
+#   filename          = data.archive_file.lambda_zip.output_path
+#   function_name     = local.name
+#   role              = aws_iam_role.lambda.arn
+#   handler           = local.lambda_handler
+#   source_code_hash  = filebase64sha256(data.archive_file.lambda_zip.output_path)
+#   runtime           = "go1.x"
+#   memory_size       = 1024
+#   timeout           = 30
 
-  environment {
-    variables = {
-      RANDOM_NAME = local.random_name
-    }
-  }
-}
+#   environment {
+#     variables = {
+#       RANDOM_NAME = local.random_name
+#     }
+#   }
+# }
