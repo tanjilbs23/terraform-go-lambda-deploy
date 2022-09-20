7a751407002c09a4bcd744e76032ab0d30723735
tanjilbs23 <tanjil.bs23@gmail.com>
2022-09-21 01:44:55 +0600

    Lambda test -12

--- lambda/hello/main.tf
+++ lambda/hello/main.tf
@@ -8,7 +8,7 @@ resource "null_resource" "lambda_build" {
     on_every_apply = uuid()
   }
   provisioner "local-exec" {
-    command = "cd ${path.cwd}/src && env GOOS=linux GOARCH=amd64 go build -o ../bin/hello"
+    command = "cd lambda/hello/src && env GOOS=linux GOARCH=amd64 go build -o ../bin/hello"
   }
 }
 
@@ -17,34 +17,34 @@ resource "null_resource" "lambda_build" {
 data "archive_file" "lambda_go_zip" {
 
   type        = "zip"
-  source_file = "${path.module}/bin/hello"
-  output_path = "${path.module}/bin/hello.zip"
+  source_file = "lambda/hello/bin/hello"
+  output_path = "lambda/hello/bin/hello.zip"
   depends_on = [
       null_resource.lambda_build
   ]
 }
 
-# # Lambda Module
-# module "lambda_function" {
-#   source        = "terraform-aws-modules/lambda/aws"
-#   function_name = "hello"
-#   description   = "testing go function"
-#   handler       = "hello.lambda_handler"
-#   runtime       = "go1.x"
+# Lambda Module
+module "lambda_function" {
+  source        = "terraform-aws-modules/lambda/aws"
+  function_name = "hello"
+  description   = "testing go function"
+  handler       = "hello.lambda_handler"
+  runtime       = "go1.x"
 
-#   create_package          = false
-#   local_existing_package  = "lambda/hello/bin/hello.zip"
+  create_package          = false
+  local_existing_package  = "${path.module}/bin/hello.zip"
 
-#   trusted_entities = [
-#     {
-#       type = "Service",
-#       identifiers = [
-#         "appsync.amazonaws.com"
-#       ]
-#     }
-#   ]
+  trusted_entities = [
+    {
+      type = "Service",
+      identifiers = [
+        "appsync.amazonaws.com"
+      ]
+    }
+  ]
 
-#   tags = {
-#     Name = "hello_go"
-#   }
-# }
+  tags = {
+    Name = "hello_go"
+  }
+}
