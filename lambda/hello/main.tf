a2bfd6507696c22eed7943bb0af14a928aeaebad
tanjilbs23 <tanjil.bs23@gmail.com>
2022-09-21 01:22:42 +0600

    Lambda test -9

--- lambda/hello/main.tf
+++ lambda/hello/main.tf
@@ -8,7 +8,7 @@ resource "null_resource" "lambda_build" {
     on_every_apply = uuid()
   }
   provisioner "local-exec" {
-    command = "cd ${path.module}/src && env GOOS=linux GOARCH=amd64 go build -o ../bin/hello"
+    command = "cd lambda/hello/src && env GOOS=linux GOARCH=amd64 go build -o ../bin/hello"
   }
 }
 
@@ -17,8 +17,8 @@ resource "null_resource" "lambda_build" {
 data "archive_file" "lambda_go_zip" {
 
   type        = "zip"
-  source_file = "${path.module}/bin/hello"
-  output_path = "${path.module}/bin/hello.zip"
+  source_file = "lambda/hello/bin/hello"
+  output_path = "lambda/hello/bin/hello.zip"
   depends_on = [
       null_resource.lambda_build
   ]
@@ -33,8 +33,8 @@ module "lambda_function" {
   runtime       = "go1.x"
 
   create_package          = false
-  local_existing_package  = "${path.module}/bin/hello.zip"
-  ignore_source_code_hash = true
+  local_existing_package  = "lambda/hello/bin/hello.zip"
+  # ignore_source_code_hash = true
 
   # source_path = "lambda/hello/bin"
   trusted_entities = [
