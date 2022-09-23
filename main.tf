c34571150fc660c737b0193c6220a97bbca93c95
tanjilbs23 <tanjil.bs23@gmail.com>
2022-09-23 16:13:59 +0600

    testing github secrets in tf files test -4

--- main.tf
+++ main.tf
@@ -6,9 +6,20 @@ terraform {
       name = "terraform-go-lambda-deploy"
     }
   }
+    required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.30.0"
+    }
+  }
+}
+
+provider "aws" {
 }
 
 module "hello" {
   source = "./lambda/hello"
   # tags   = "dev"
 }
+
+
