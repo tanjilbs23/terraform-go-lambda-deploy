fa2c6b5d117c65ab4b975a1e538a0c0db9a8e95a
tanjilbs23 <tanjil.bs23@gmail.com>
2022-09-23 16:07:03 +0600

    testing github secrets in tf files test -2

--- lambda/hello/main.tf
+++ lambda/hello/main.tf
@@ -18,20 +18,21 @@ data "archive_file" "lambda_go_zip" {
   source_file = "${path.module}/bin/hello"
   output_path = "${path.module}/bin/hello.zip"
   depends_on = [
-      null_resource.lambda_build
+    null_resource.lambda_build
   ]
 }
 
 # Lambda Module
 module "lambda_function" {
   source        = "terraform-aws-modules/lambda/aws"
+  version       = "4.0.1"
   function_name = "hello"
   description   = "testing go function"
   handler       = "hello.lambda_handler"
   runtime       = "go1.x"
 
-  create_package          = false
-  local_existing_package  = "${path.module}/bin/hello.zip"
+  create_package         = false
+  local_existing_package = "${path.module}/bin/hello.zip"
 
   trusted_entities = [
     {
@@ -43,7 +44,7 @@ module "lambda_function" {
   ]
 
   tags = {
-    Name =var.tags
+    Name = var.tags
   }
 
   depends_on = [
