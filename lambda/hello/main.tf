ee46f69397bb7d28504c631e8d860f4651150f8b
tanjilbs23 <tanjil.bs23@gmail.com>
2022-10-12 19:18:47 +0600

    new test - 35

--- lambda/hello/main.tf
+++ lambda/hello/main.tf
@@ -1,18 +1,18 @@
 resource "null_resource" "lambda_build" {
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
   # triggers = {
   #   on_every_apply = uuid()
   # }
-  triggers = {
-    file_hashes = jsonencode({
-      for fn in fileset("${path.module}/src", "**") :
-      fn => filesha256("${path.module}/src/${fn}")
-    })
-  }
+  # triggers = {
+  #   file_hashes = jsonencode({
+  #     for fn in fileset("${path.module}/src", "**") :
+  #     fn => filesha256("${path.module}/src/${fn}")
+  #   })
+  # }
   provisioner "local-exec" {
-    command = "cd ${path.module}/src && go build -o ../bin/handler && cd ../bin && ls -la && zip handler.zip handler && ls -la"
+    command = "cd ${path.module}/src && go build -o ${path.module}/bin/handler && cd ${path.module}/bin && ls -la && zip handler.zip handler && ls -la"
   }
 }
 
