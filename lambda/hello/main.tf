2921fc94a1b6ac3bfe8b88893955db52046267ac
tanjilbs23 <tanjil.bs23@gmail.com>
2022-10-13 14:48:32 +0600

    test 51

--- lambda/hello/main.tf
+++ lambda/hello/main.tf
@@ -1,20 +1,20 @@
-resource "null_resource" "lambda_build" {
-  triggers = {
-    always_run = "${timestamp()}"
-  }
-  # triggers = {
-  #   on_every_apply = uuid()
-  # }
-  # triggers = {
-  #   file_hashes = jsonencode({
-  #     for fn in fileset("${path.module}/src", "**") :
-  #     fn => filesha256("${path.module}/src/${fn}")
-  #   })
-  # }
-  provisioner "local-exec" {
-    command = "cd ${path.module}/src && go build -o ../bin/handler"
-  }
-}
+# resource "null_resource" "lambda_build" {
+#   triggers = {
+#     always_run = "${timestamp()}"
+#   }
+#   # triggers = {
+#   #   on_every_apply = uuid()
+#   # }
+#   # triggers = {
+#   #   file_hashes = jsonencode({
+#   #     for fn in fileset("${path.module}/src", "**") :
+#   #     fn => filesha256("${path.module}/src/${fn}")
+#   #   })
+#   # }
+#   provisioner "local-exec" {
+#     command = "cd ${path.module}/src && go build -o ../bin/handler"
+#   }
+# }
 
 data "archive_file" "lambda_go_zip" {
 
